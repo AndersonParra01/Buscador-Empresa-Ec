b1ae59b34f4dd26725552f9e7909ceed76fb5470
Anderson <ajr.parra@yavirac.edu.ec>
2025-02-11 14:04:25 -0500

    agregacion del search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,6 +3,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { ChipModule } from 'primeng/chip';
+import { TabsModule } from 'primeng/tabs';
+import { AvatarModule } from 'primeng/avatar';
+import { BadgeModule } from 'primeng/badge';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+
 @Component({
   selector: 'app-search',
   imports: [
@@ -11,17 +16,21 @@ import { ChipModule } from 'primeng/chip';
     ReactiveFormsModule,
     FormsModule,
     ChipModule,
+    TabsModule,
+    AvatarModule,
+    BadgeModule,
+    OverlayBadgeModule,
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
 export class SearchComponent {
-  searchText: string = ''; // Para el nombre de la empresa
+  searchCompany: string = ''; // Para el nombre de la empresa
   searchRUC: string = ''; // Para búsqueda por RUC
-  selectedCities: string[] = []; // Ciudades seleccionadas
-  selectedNeighborhoods: string[] = []; // Barrios seleccionados
-  cities: string[] = ['Ciudad 1', 'Ciudad 2', 'Ciudad 3'];
-  neighborhoods: string[] = ['Barrio A', 'Barrio B', 'Barrio C'];
+  //selectedCities: string[] = []; // Ciudades seleccionadas
+  //selectedNeighborhoods: string[] = []; // Barrios seleccionados
+  //cities: string[] = ['Ciudad 1', 'Ciudad 2', 'Ciudad 3'];
+  //neighborhoods: string[] = ['Barrio A', 'Barrio B', 'Barrio C'];
 
   search() {
     console.log('Buscar por:');
